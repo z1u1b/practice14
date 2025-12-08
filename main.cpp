08176039b924b2cc29cb9c0d8ed527c1fe237819
zub <ars1for1work@gmail.com>
2025-12-08 17:23:43 +0300

    Work version with dots

--- main.cpp
+++ main.cpp
@@ -1,84 +1,193 @@
 #include <iostream>
-using s_t = size_t;
-
-namespace top
+namespace topit
 {
   struct p_t
   {
     int x, y;
   };
+  bool operator==(p_t, p_t);
+  bool operator!=(p_t, p_t);
+  struct f_t
+  {
+    p_t aa, bb;
+  };
 
   struct IDraw
   {
+    virtual ~IDraw() = default;
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
-    virtual ~IDraw() = default;
   };
-  bool operator==(p_t a, p_t b)
-  {
-    return a.x == b.x && a.y == b.y;
-  }
-  bool operator!=(p_t a, p_t b)
-  {
-    return !(a == b);
-  }
 
-  struct Dot:IDraw
+  struct Dot : IDraw
   {
+    Dot(int x, int y);
+    explicit Dot(p_t dd);
     p_t begin() const override;
     p_t next(p_t) const override;
-    p_t o;
-    Dot(int x,int y);
+    p_t d;
   };
-  struct frame_f {
-    p_t left_dot;
-    p_t right_dot;
-  };
-  void make_f(IDraw ** b,s_t k);
-  void get_points(IDraw * b,p_t **ps,s_t &s);
-  frame_f build_frame(const p_t * ps, s_t s);
-  char * build_canvas(frame_f f);
-  void paint_canvas(char * cnv,frame_f fr,const p_t * ps,s_t k,char f);
-  void print_canvas(const char * cnv,frame_f fr);
-} // namespace top
+  // Домашнее задание:
+  // - Добавить ещё 2-3 фигуры:
+  //   - Вертикальный отрезок
+  //   - Горизонтальный отрезок
+  //   - Диагональ под 45 заданной длины
+  //   - Придумать свою фигуру
 
+  // расширять заданный массив точками из очередной фигуры
+  // - extend...
+  size_t points(const IDraw& d, p_t** pts, size_t s);
+
+  // найти минимум и максимум по каждой координате среди точек и сформировать фрейм
+  f_t frame(const p_t* pts, size_t s);
+
+  // построить полотно (из фрейма получить количество столбцов и колонок)
+  char* canvas(f_t fr, char fill);
+
+  // координаты точки перевести в координаты в двумерном массиве
+  void paint(char* cnv, f_t fr, p_t p, char fill);
+
+  // вывод двумперного массива на основе размеров, определяемых фреймом
+  void flush(std::ostream& os, const char* cnv, f_t fr);
+
+  void extend(topit::p_t** pts, size_t s, p_t p)
+  {
+    size_t upd_s = s + 1;
+    p_t* res = new p_t[upd_s];
+    for (size_t i = 0; i < s; ++i) {
+      res[i] = (*pts)[i];
+    }
+
+    res[s] = p;
+    delete[] *pts;
+    *pts = res;
+  }
+  size_t rows(f_t fr);
+  size_t cols(f_t fr);
+} // namespace topit
 int main()
 {
-  using namespace top;
-  int err=0;
-  IDraw * f[3]={};
-  p_t *p = nullptr;
-  s_t s=0;
-  char * cnv=nullptr;
+  using topit::Dot;
+  using topit::f_t;
+  using topit::IDraw;
+  using topit::p_t;
+  int err = 0;
+  IDraw* shps[3] = {};
+  p_t* pts = nullptr;
+  size_t s = 0;
   try {
-    make_f(f,3);
-    for (s_t i=0;i<3;++i) {
-      get_points(f[i],&p,s);
+    shps[0] = new Dot(0, 0);
+    shps[1] = new Dot(5, 7);
+    shps[2] = new Dot(-5, -2);
+    for (size_t i = 0; i < 3; ++i) {
+      s += topit::points(*(shps[i]), &pts, s);
     }
-    frame_f fr = build_frame(p,s);
-
-    cnv=build_canvas(fr);
-    paint_canvas(cnv,fr,p,s,'#');
-    print_canvas(cnv,fr);
+    f_t fr = frame(pts, s);
+    char* cnv = topit::canvas(fr, '.');
+    for (size_t i = 0; i < s; ++i) {
+      topit::paint(cnv, fr, pts[i], '#');
+    }
+    topit::flush(std::cout, cnv, fr);
+    delete[] cnv;
   } catch (...) {
-    err=1;
+    err = 2;
+    std::cerr << "Bad drawing\n";
   }
-  // for (s_t i=0;i<3;++i){
-  //   delete f[i];
-  // }
-  delete f[0];
-  delete f[1];
-  delete f[2];
-  delete [] p;
-  delete [] cnv;
+  delete[] pts;
+  delete shps[0];
+  delete shps[1];
+  delete shps[2];
+  return err;
 }
-top::Dot::Dot(int x,int y):
-  IDraw(),
-  o{x,y}
+
+size_t topit::points(const IDraw& d, p_t** pts, size_t s)
+{
+  p_t p = d.begin();
+  extend(pts, s, p);
+  size_t delta = 1;
+  while (d.next(p) != d.begin()) {
+    p = d.next(p);
+    extend(pts, s + delta, p);
+    ++delta;
+  }
+  return delta;
+}
+
+topit::f_t topit::frame(const p_t* pts, size_t s)
+{
+  if (!s) {
+    throw std::logic_error("bad size");
+  }
+  int minx = pts[0].x, maxx = minx;
+  int miny = pts[0].y, maxy = miny;
+  for (size_t i = 1; i < s; ++i) {
+    minx = std::min(minx, pts[i].x);
+    maxx = std::max(maxx, pts[i].x);
+    miny = std::min(miny, pts[i].y);
+    maxy = std::max(maxy, pts[i].y);
+  }
+  p_t aa{minx, miny};
+  p_t bb{maxx, maxy};
+  return {aa, bb};
+}
+size_t topit::rows(topit::f_t fr)
+{
+  return (fr.bb.y - fr.aa.y + 1);
+}
+size_t topit::cols(topit::f_t fr)
+{
+  return (fr.bb.x - fr.aa.x + 1);
+}
+char* topit::canvas(topit::f_t fr, char fill)
+{
+  char* cnv = new char[rows(fr) * cols(fr)];
+  for (size_t i = 0; i < rows(fr) * cols(fr); ++i) {
+    cnv[i] = fill;
+  }
+  return cnv;
+}
+
+void topit::paint(char* cnv, topit::f_t fr, topit::p_t p, char fill)
+{
+  int dx = p.x - fr.aa.x;
+  int dy = fr.bb.y - p.y;
+  cnv[dy * cols(fr) + dx] = fill;
+}
+
+void topit::flush(std::ostream& os, const char* cnv, topit::f_t fr)
+{
+  for (size_t i = 0; i < rows(fr); ++i) {
+    for (size_t j = 0; j < cols(fr); ++j) {
+      os << cnv[i * cols(fr) + j];
+    }
+    os << '\n';
+  }
+}
+
+topit::Dot::Dot(p_t dd) : IDraw(), d{dd}
 {}
-top::p_t top::Dot::begin() const {
-  return o;
+
+topit::Dot::Dot(int x, int y) : IDraw(), d{x, y}
+{}
+
+topit::p_t topit::Dot::begin() const
+{
+  return d;
 }
-top::p_t top::Dot::next(p_t) const {
-  return begin();
+
+topit::p_t topit::Dot::next(p_t prev) const
+{
+  if (prev != begin()) {
+    throw std::logic_error("bad impl");
+  }
+  return d;
+}
+
+bool topit::operator==(p_t a, p_t b)
+{
+  return a.x == b.x && a.y == b.y;
+}
+bool topit::operator!=(p_t a, p_t b)
+{
+  return !(a == b);
 }
